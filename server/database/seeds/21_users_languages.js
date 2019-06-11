b7d84d7554345351c9264bc9cc88e3db096b9ff8
Frank Heggeness <47344070+frankheggeness@users.noreply.github.com>
2019-06-10 18:28:46 -1000

    Update 21_users_languages.js

--- server/database/seeds/21_users_languages.js
+++ server/database/seeds/21_users_languages.js
@@ -5,12 +5,18 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('users_languages').insert([
-        { user_id: 1, language_id: 1, language_type: 'primary' },
-        { user_id: 2, language_id: 1, language_type: 'primary' },
-        { user_id: 3, language_id: 2, language_type: 'primary' },
-        { user_id: 4, language_id: 2, language_type: 'primary' },
-        { user_id: 5, language_id: 1, language_type: 'primary' },
-        { user_id: 5, language_id: 2, language_type: 'primary' },
+        { user_id: 1, language_id: 1, language_type: 'target' },
+        { user_id: 2, language_id: 1, language_type: 'target' },
+        { user_id: 3, language_id: 2, language_type: 'target' },
+        { user_id: 4, language_id: 2, language_type: 'target' },
+        { user_id: 5, language_id: 1, language_type: 'target' },
+        { user_id: 6, language_id: 2, language_type: 'target' },
+        { user_id: 1, language_id: 3, language_type: 'native' },
+        { user_id: 2, language_id: 3, language_type: 'native' },
+        { user_id: 3, language_id: 3, language_type: 'native' },
+        { user_id: 4, language_id: 3, language_type: 'native' },
+        { user_id: 5, language_id: 3, language_type: 'native' },
+        { user_id: 6, language_id: 3, language_type: 'native' },
       ]);
     });
 };
