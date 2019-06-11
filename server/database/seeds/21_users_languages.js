66a0fd8a34847a58cc62f7ba5bdd24359c44f9dd
Frank Heggeness <47344070+frankheggeness@users.noreply.github.com>
2019-06-10 18:30:22 -1000

    Update 21_users_languages.js

--- server/database/seeds/21_users_languages.js
+++ server/database/seeds/21_users_languages.js
@@ -10,13 +10,13 @@ exports.seed = function(knex, Promise) {
         { user_id: 3, language_id: 2, language_type: 'target' },
         { user_id: 4, language_id: 2, language_type: 'target' },
         { user_id: 5, language_id: 1, language_type: 'target' },
-        { user_id: 6, language_id: 2, language_type: 'target' },
+        { user_id: 6, language_id: 3, language_type: 'target' },
         { user_id: 1, language_id: 3, language_type: 'native' },
         { user_id: 2, language_id: 3, language_type: 'native' },
         { user_id: 3, language_id: 3, language_type: 'native' },
         { user_id: 4, language_id: 3, language_type: 'native' },
         { user_id: 5, language_id: 3, language_type: 'native' },
-        { user_id: 6, language_id: 3, language_type: 'native' },
+        { user_id: 6, language_id: 1, language_type: 'native' },
       ]);
     });
 };
