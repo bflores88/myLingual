44606db4b076e2c371905e2a044e033f03791aa2
Frank <fr.heggeness@gmail.com>
2019-06-10 14:30:03 -1000

    started on seeds

--- server/database/seeds/03_words.js
+++ server/database/seeds/03_words.js
@@ -1,13 +1,9 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('words')
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex('words').insert([{ english_word: 'banana' }, { english_word: 'chain' }, { english_word: 'camel' }]);
     });
 };
