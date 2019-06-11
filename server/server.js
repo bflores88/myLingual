6072cdca3015a92f5701246cbe4454bbe16c3798
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-06-11 10:22:36 -1000

    websockets template

--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@
 
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({ path: '../.env' });
 
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
 
 const cards = require('./routes/cards');
 const decks = require('./routes/decks');
 const users = require('./routes/users');
+const conversations = require('./routes/conversations');
 
 app.use(bodyParser.json());
 app.use(
@@ -23,11 +26,8 @@ app.use(express.static('public'));
 app.use('/api/cards', cards);
 app.use('/api/decks', decks);
 app.use('/api/users', users);
+app.use('/api/conversations', conversations);
 
-app.use('/', (req, res) => {
-  res.send('smoke test')
-})
-
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Express app is running at port ${PORT}`);
-})
+});
