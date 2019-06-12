df3a4c7cd13d234de78363cb804303be2c79f944
kmietotwy <kmietotwy@gmail.com>
2019-06-11 19:28:19 -1000

    fixed profile image sizing

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from 'src/app/services/backend.services';
+import { Router } from '@angular/router';
+
+interface UserResponse {
+  id: number;
+  active: boolean;
+  role_id: number,
+  role: string,
+  name: string;
+  username: string;
+  email: string;
+  profile_image_url: string;
+  created_at: string;
+  cards_owned: number;
+  cards_created: number;
+  decks: number;
+  native_languages: object;
+  target_languages: object;
+
+}
 
 @Component({
   selector: 'app-profile',
@@ -6,19 +26,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  userID = 4;
 
   user: {
     id: number;
+    active: boolean;
+    role_id: number,
+    role: string,
     name: string;
     username: string;
-    primaryLanguage: string;
-    learningLanguages: string;
-    imageLink: string;
+    email: string;
+    profile_image_url: string;
+    created_at: string;
+    cards_owned: number;
+    cards_created: number;
+    decks: number;
+    native_languages: object;
+    target_languages: object;
   };
 
-  constructor() { }
+
+  constructor(
+    private backend: BackendService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    return this.getUser();
+  }
+
+  getUser() {
+    this.backend.getUserProfile(this.userID).then((data: UserResponse) => {
+      this.user = data;
+
+      console.log(data);
+    })
   }
 
 }
