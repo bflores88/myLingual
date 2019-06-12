0f8901d8db7c8b1a57aedc247dc19c7d36bf5f1e
kmietotwy <kmietotwy@gmail.com>
2019-06-12 10:11:24 -1000

    basic login working

--- server/server.js
+++ server/server.js
@@ -5,12 +5,21 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcryptjs');
+const redis = require('connect-redis')(session);
+
+const User = require('./database/models/User');
 
 require('dotenv').config({ path: '../.env' });
 
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
 
 const login = require('./routes/login');
+const logout = require('./routes/logout');
 const cards = require('./routes/cards');
 const decks = require('./routes/decks');
 const users = require('./routes/users');
@@ -29,7 +38,87 @@ app.use(
 );
 app.use(express.static('public'));
 
+app.use(cookieParser());
+app.use(
+  session({
+    store: new redis({ url: process.env.REDIS_URL }),
+    secret: process.env.REDIS_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    return new User({ username: username })
+      .fetch()
+      .then((user) => {
+        if (user === null) {
+          return done(null, false, { message: 'bad username or password' });
+        } else {
+          user = user.toJSON();
+
+          bcrypt
+            .compare(password, user.password)
+            .then((res) => {
+              //Happy route: username exists, password matches
+              if (res) {
+                return done(null, user);
+              }
+
+              //Error route: Username exists, password does not match
+              else {
+                return done(null, false, { message: 'bad username or password' });
+              }
+            })
+            .catch((err) => {
+              console.log('err', err);
+              return done(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log('err', err);
+        return done(err);
+      });
+  }),
+);
+
+passport.serializeUser(function(user, done) {
+  console.log('serializing');
+  return done(null, { id: user.id, username: user.username});
+});
+
+passport.deserializeUser(function(user, done) {
+  console.log('deserializing');
+
+  return new User({ id: user.id })
+    .fetch()
+    .then((user) => {
+      user = user.toJSON();
+
+      done(null, {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        active: user.active,
+        role_id: user.role_id,
+        name: user.name,
+      });
+    })
+    .catch((err) => {
+      console.log('err', err);
+      return done(err);
+    });
+});
+
 app.use('/api/login', login);
+
 app.use('/api/cards', cards);
 app.use('/api/decks', decks);
 app.use('/api/users', users);
