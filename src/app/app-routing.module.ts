2f0a10e91b3e724cec4efd9d4eb4efa7fcad63d6
Frank <fr.heggeness@gmail.com>
2019-06-13 18:37:45 -1000

    added language code, lingots, deck to quiz tables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,14 +15,13 @@ import { ForumPostComponent } from './pages/forum-post/forum-post.component';
 import { NewForumPostComponent } from './pages/new-forum-post/new-forum-post.component';
 import { AddCardNoImageComponent } from './pages/add-card-no-image/add-card-no-image.component';
 
-import { AddCardImageUploadComponent } from './add-card-image-upload/add-card-image-upload.component';
+// import { AddCardImageUploadComponent } from './add-card-image-upload/add-card-image-upload.component';
 
 import { TestComponent } from './pages/test/test.component';
 
 import { UserSettingsComponent } from './pages/user-settings/user-settings.component';
 import { DeckDetailComponent } from './pages/deck-detail/deck-detail.component';
 import { AddCardUploadComponent } from './pages/add-card-upload/add-card-upload.component';
-
 
 const routes: Routes = [
   { path: '', component: SplashComponent },
