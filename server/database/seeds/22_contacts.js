c100e7f7b9704c28583d2e2477e133843731f9a2
Frank <fr.heggeness@gmail.com>
2019-06-14 14:00:02 -1000

    basic contacts page

--- server/database/seeds/22_contacts.js
+++ server/database/seeds/22_contacts.js
@@ -9,8 +9,9 @@ exports.seed = function(knex, Promise) {
         { requester: 1, invitee: 3, accepted: true, responded: true },
         { requester: 1, invitee: 4, accepted: false, responded: false },
         { requester: 2, invitee: 3, accepted: true, responded: true },
-        { requester: 2, invitee: 1, accepted: true, responded: true },
+        { requester: 5, invitee: 1, accepted: true, responded: true },
         { requester: 3, invitee: 5, accepted: true, responded: true },
+        { requester: 6, invitee: 1, accepted: true, responded: true },
       ]);
     });
 };
