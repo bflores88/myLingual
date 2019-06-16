b02c01d663c41318ae917b3a7f4edda2c4a74634
Luke Fiorio <lukefiorio@Lukes-MBP.lan>
2019-06-15 19:51:02 -1000

    added guards, error handling on sending invites, routes to cancel invite/remove contact

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const Contact = require('../database/models/Contact');
-
-const knex = require('../database/knex.js');
+const authGuard = require('../guards/authGuard');
+const invitesGuard = require('../guards/invitesGuard');
+const requestsGuard = require('../guards/requestsGuard');
+const contactsGuard = require('../guards/contactsGuard');
 
 // get user's accepted contacts
-router.route('/').get((req, res) => {
+router.route('/').get(authGuard, (req, res) => {
   Contact.query({
     where: { requester: req.user.id, accepted: true },
     orWhere: { invitee: req.user.id, accepted: true },
@@ -25,7 +27,7 @@ router.route('/').get((req, res) => {
 
 router
   .route('/invites')
-  .get((req, res) => {
+  .get(authGuard, (req, res) => {
     Contact.where({ invitee: req.user.id, responded: false })
       .fetchAll({ withRelated: ['requesters', 'invitees'] })
       .then((result) => {
@@ -37,15 +39,58 @@ router
         return res.status(500).send('Server error');
       });
   })
-  // send an invite
-  .post((req, res) => {
-    // TO ADD: error checking to ensure invitation is valid
-    new Contact()
+  .post(authGuard, (req, res) => {
+    if (parseInt(req.body.invitee) === req.user.id) {
+      console.log('Cannot send an invite to self');
+      return res.send('Server error');
+    }
+
+    // fetch all contact records associated with user & invitee
+    Contact.query({
+      where: { requester: req.user.id, invitee: req.body.invitee },
+      orWhere: { requester: req.body.invitee, invitee: req.user.id },
+    })
+      .fetchAll()
+      .then((result) => {
+        let invalidInvite = false;
+        // validate there are no accepted or outstanding invites
+        if (result.length > 0) {
+          result.toJSON().forEach((contact) => {
+            if (contact.accepted || !contact.responded) {
+              invalidInvite = true;
+            }
+          });
+        }
+
+        if (invalidInvite) {
+          throw new Error('Invalid invite.');
+        }
+
+        // send an invite
+        return new Contact().save({
+          requester: req.user.id,
+          invitee: req.body.invitee,
+          accepted: false,
+          responded: false,
+        });
+      })
+      .then((result) => {
+        return res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        return res.status(500).send('Server error');
+      });
+  });
+
+router
+  .route('/invites/:id')
+  .put(authGuard, invitesGuard, (req, res) => {
+    // respond to invitation
+    new Contact('id', req.params.id)
       .save({
-        requester: req.user.id,
-        invitee: req.body.invitee,
-        accepted: false,
-        responded: false,
+        accepted: req.body.accepted,
+        responded: true,
       })
       .then((result) => {
         return res.json(result);
@@ -54,18 +99,28 @@ router
         console.log('error', err);
         return res.status(500).send('Server error');
       });
+  })
+  .delete(authGuard, requestsGuard, (req, res) => {
+    // cancel invite
+    Contact.where({ id: req.params.id })
+      .destroy()
+      .then(() => {
+        return res.send('Successful delete');
+      })
+      .catch((err) => {
+        console.log('error', err);
+        return res.status(500).send('Server error');
+      });
   });
 
-// respond to open invitations
-router.route('/invites/:id').put((req, res) => {
+router.route('/:id').delete(authGuard, contactsGuard, (req, res) => {
+  // remove contact (swap accepted to false)
   new Contact('id', req.params.id)
     .save({
-      accepted: req.body.accepted,
-      responded: true,
+      accepted: false,
     })
-    .then((result) => {
-      // TO ADD: if accepted, respond with new contact info
-      return res.json(result);
+    .then(() => {
+      return res.send('Succesful removal');
     })
     .catch((err) => {
       console.log('error', err);
