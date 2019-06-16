e9df7bfb761a30e260862d3ef6ba5c9bde615ec7
Ronald J Nagata Jr <rnagata@thetahawk.com>
2019-06-15 15:53:46 -1000

    Mvp?

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from 'src/app/services/backend.services';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
 
 interface ResponseData {
   errorMessage: string;
   english_word: string;
+  created_by: number;
   spanish_translations: string;
   italian_translations: string;
+}
+
+interface Contact {
+  id: number;
+}
+
+interface Creator {
+  id: number;
 }
 
 @Component({
@@ -16,16 +26,40 @@ interface ResponseData {
 })
 
 export class CardComponent implements OnInit {
-  flashcard: Object;
-  errorMessage: String;
+  errorMessage: string;
 
+  id: string;
+  flashcard: object;
+  creator: Creator;
   hasFlashcard = false;
+  hasCreator = false;
+  hasRelation: boolean;
+  
+  user: {
+    loggedIn: boolean;
+    username: string;
+    id: any;
+  };
 
-  constructor(private backend: BackendService, private route: ActivatedRoute) { }
+  constructor(
+    private backend: BackendService, 
+    private activatedRoute: ActivatedRoute,
+    private router: Router, 
+    private session: SessionService
+  ) {}
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.backend.getFlashcard(id)
+    this.activatedRoute.paramMap.subscribe((routeParams: ParamMap) => {
+      this.loadCard();
+    });
+  }
+
+  loadCard() {
+    // console.log('getting user session ', this.session.getSession());
+    this.user = this.session.getSession();
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.backend.getFlashcard(this.id)
     .then((response: ResponseData) => {
       if (response.errorMessage) {
         this.hasFlashcard = false;
@@ -34,14 +68,70 @@ export class CardComponent implements OnInit {
         const englishWordCaptial = response.english_word.slice(0, 1).toUpperCase();
         const englishWordRemainder = response.english_word.slice(1);
         response.english_word = englishWordCaptial + englishWordRemainder;
-        this.hasFlashcard = true;
         this.flashcard = response;
-        console.log(this.flashcard);
+        this.hasFlashcard = true;
+        // console.log('flashcard retrieved ', this.flashcard);
       }
+
+      this.backend.getUserProfile(response.created_by)
+      .then((result: Creator) => {
+        this.creator = result;
+        this.hasCreator = true;
+        // console.log('flashcard creator ', result);
+
+        if (this.user.loggedIn) {
+          // console.log('known user visits page');
+          this.backend.getUserContacts()
+          .then((result: []) => {
+            this.hasRelation = false;
+            result.forEach((contact: Contact) => {
+              // console.log('contact ', contact);
+              if (contact.id === this.creator.id){
+                this.hasRelation = true;
+              }
+            })
+            // if (this.hasRelation){
+              // console.log('the creator is the users friend');
+            // } else {
+              // console.log('the user has no relation to the creator');
+            // }
+          })
+          .catch((error) => {
+            this.errorMessage = 'Error getting contacts.';
+            // console.log('error getting contacts ', error);
+          });
+        }
+        // } else {
+        //   console.log('anonymous person visits page');
+        // }
+      })
+      .catch(() => {
+        this.errorMessage = 'Error retrieving creator details.';
+        // console.log('hasCreator: ', this.hasCreator);
+      });
     })
     .catch(() => {
-      this.hasFlashcard = false;
       this.errorMessage = 'Error retrieving card.';
-    })
+      console.log('hasFlashcard: ', this.hasFlashcard);
+    });
+  }
+
+  nextCard() {
+    // replace later
+    let newParam = parseInt(this.id) + 1;
+    if (newParam <= 5){
+      console.log('nextCard()');
+      this.router.navigate([`/card/${newParam}`]);
+    }
+  }
+
+  previousCard() {
+    // replace later
+    let newParam = parseInt(this.id) - 1;
+    if (newParam > 0){
+      console.log('previousCard()');
+      this.router.navigate([`/card/${newParam}`]);
+    }
+    
   }
 }
