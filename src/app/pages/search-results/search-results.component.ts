ca1080e833062e99c5f700ad059b91299716737f
Luke Fiorio <lukefiorio@Lukes-MBP.lan>
2019-06-17 12:54:57 -1000

    boiler plate for unformatted search results page

--- src/app/pages/search-results/search-results.component.ts
+++ src/app/pages/search-results/search-results.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.services';
+import { ActivatedRoute } from '@angular/router';
+
+interface SearchMatches {
+  match_id: number;
+  match_name: string;
+  match_score: number;
+  match_image: string;
+  match_own: number;
+  match_type: string;
+  sortScore: number;
+}
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
+  searchMatches: SearchMatches[];
 
-  constructor() { }
+  constructor(private backend: BackendService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    const search_text = this.route.snapshot.paramMap.get('search_text');
+    this.backend.search(search_text).then((data: SearchMatches[]) => {
+      this.searchMatches = data;
+    });
   }
-
 }
