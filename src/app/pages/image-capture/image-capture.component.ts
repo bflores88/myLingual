1bbd102203e8d239f7c5513c23066f31cb8b02c7
Frank <fr.heggeness@gmail.com>
2019-06-17 10:38:51 -1000

    potential session fix

--- src/app/pages/image-capture/image-capture.component.ts
+++ src/app/pages/image-capture/image-capture.component.ts
@@ -24,13 +24,15 @@ export class ImageCaptureComponent implements OnInit {
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
       let videoTracks = stream.getVideoTracks();
 
-      window.stream = stream;
+      // window.stream = stream;
       video.srcObject = stream;
     });
   }
 
   public capture() {
-    let context = this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0, canvas.width, canvas.height);
+    let context = this.canvas.nativeElement
+      .getContext('2d')
+      .drawImage(this.video.nativeElement, 0, 0, canvas.width, canvas.height);
     this.captures.push(this.canvas.nativeElement.toDataURL('images/png'));
   }
 }
