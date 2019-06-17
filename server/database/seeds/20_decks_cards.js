2001126a42d1ff8e42da3f82da86bd9128dc635e
Luke Fiorio <lukefiorio@Lukes-MBP.lan>
2019-06-17 10:14:54 -1000

    moved card_theme_id from cards to decks_cards

--- server/database/seeds/20_decks_cards.js
+++ server/database/seeds/20_decks_cards.js
@@ -5,14 +5,14 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('decks_cards').insert([
-        { users_cards_id: 1, deck_id: 1 },
-        { users_cards_id: 2, deck_id: 1 },
-        { users_cards_id: 3, deck_id: 1 },
-        { users_cards_id: 4, deck_id: 1 },
-        { users_cards_id: 5, deck_id: 1 },
-        { users_cards_id: 6, deck_id: 2 },
-        { users_cards_id: 7, deck_id: 2 },
-        { users_cards_id: 8, deck_id: 2 },
+        { users_cards_id: 1, deck_id: 1, card_theme_id: 1 },
+        { users_cards_id: 2, deck_id: 1, card_theme_id: 1 },
+        { users_cards_id: 3, deck_id: 1, card_theme_id: 1 },
+        { users_cards_id: 4, deck_id: 1, card_theme_id: 1 },
+        { users_cards_id: 5, deck_id: 1, card_theme_id: 1 },
+        { users_cards_id: 6, deck_id: 2, card_theme_id: 1 },
+        { users_cards_id: 7, deck_id: 2, card_theme_id: 1 },
+        { users_cards_id: 8, deck_id: 2, card_theme_id: 1 },
       ]);
     });
 };
