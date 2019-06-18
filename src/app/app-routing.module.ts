11b63ee816cb8b5559b9db343f4e117867b7a2cb
kmietotwy <kmietotwy@gmail.com>
2019-06-17 17:10:47 -1000

    front end auth guards added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,32 +23,34 @@ import { ContactsComponent } from './pages/contacts/contacts.component';
 import { CardComponent } from './pages/card/card.component';
 import { InvitesComponent } from './pages/invites/invites.component';
 import { SearchResultsComponent } from './pages/search-results/search-results.component';
+import { AuthGuard } from './guards/auth-guard.service';
+
 
 const routes: Routes = [
   { path: '', component: SplashComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', canActivate: [AuthGuard], component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile/:user_id', component: ProfileComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'contacts', component: ContactsComponent },
-  { path: 'invites', component: InvitesComponent },
-  { path: 'settings', component: UserSettingsComponent },
-  { path: 'decks', component: DeckComponent },
-  { path: 'decks/:id', component: DeckDetailComponent },
+  { path: 'profile/:user_id', canActivate: [AuthGuard], component: ProfileComponent },
+  { path: 'profile', canActivate: [AuthGuard], component: ProfileComponent },
+  { path: 'contacts', canActivate: [AuthGuard], component: ContactsComponent },
+  { path: 'invites', canActivate: [AuthGuard], component: InvitesComponent },
+  { path: 'settings', canActivate: [AuthGuard], component: UserSettingsComponent },
+  { path: 'decks', canActivate: [AuthGuard], component: DeckComponent },
+  { path: 'decks/:id', canActivate: [AuthGuard],  component: DeckDetailComponent },
   { path: 'card/:id', component: CardComponent },
-  { path: 'test/:id', component: TestComponent },
-  { path: 'add-card', component: AddCardComponent },
-  { path: 'add-card/continue', component: AddCardNoImageComponent },
-  { path: 'add-card/upload', component: AddCardUploadComponent },
-  { path: 'add-card/capture', component: ImageCaptureComponent },
-  { path: 'messages', component: ConversationsComponent },
-  { path: 'messages/:id', component: MessagesComponent },
-  { path: 'forums', component: ForumsComponent },
-  { path: 'forums/:id', component: ForumTopicComponent },
-  { path: 'forums/new/:id', component: NewForumPostComponent },
-  { path: 'forums/posts/:post_id', component: ForumPostComponent },
-  { path: 'search/:search_text', component: SearchResultsComponent },
+  { path: 'test/:id', canActivate: [AuthGuard], component: TestComponent },
+  { path: 'add-card', canActivate: [AuthGuard], component: AddCardComponent },
+  { path: 'add-card/continue', canActivate: [AuthGuard], component: AddCardNoImageComponent },
+  { path: 'add-card/upload', canActivate: [AuthGuard], component: AddCardUploadComponent },
+  { path: 'add-card/capture', canActivate: [AuthGuard], component: ImageCaptureComponent },
+  { path: 'messages', canActivate: [AuthGuard], component: ConversationsComponent },
+  { path: 'messages/:id', canActivate: [AuthGuard], component: MessagesComponent },
+  { path: 'forums', canActivate: [AuthGuard], component: ForumsComponent },
+  { path: 'forums/:id', canActivate: [AuthGuard], component: ForumTopicComponent },
+  { path: 'forums/new/:id', canActivate: [AuthGuard], component: NewForumPostComponent },
+  { path: 'forums/posts/:post_id', canActivate: [AuthGuard], component: ForumPostComponent },
+  { path: 'search/:search_text', canActivate: [AuthGuard], component: SearchResultsComponent },
 ];
 
 @NgModule({
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,16 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    // auth2 is initialized with gapi.auth2.init() and a user is signed in.
-    let auth2: any;
-
-    if (auth2.isSignedIn.get()) {
-      var profile = auth2.currentUser.get().getBasicProfile();
-      console.log('ID: ' + profile.getId());
-      console.log('Full Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail());
-    }
-  }
+  ngOnInit() {}
 }
