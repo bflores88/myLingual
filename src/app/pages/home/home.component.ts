0b0feb2a8b7ceec875a0b1deab4da0247964615e
Luke Fiorio <lukefiorio@Lukes-MBP.lan>
2019-06-17 17:16:53 -1000

    commented oauth code generating errors

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,14 +10,13 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     // auth2 is initialized with gapi.auth2.init() and a user is signed in.
-    let auth2: any;
-
-    if (auth2.isSignedIn.get()) {
-      var profile = auth2.currentUser.get().getBasicProfile();
-      console.log('ID: ' + profile.getId());
-      console.log('Full Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail());
-    }
+    // let auth2: any;
+    // if (auth2.isSignedIn.get()) {
+    //   var profile = auth2.currentUser.get().getBasicProfile();
+    //   console.log('ID: ' + profile.getId());
+    //   console.log('Full Name: ' + profile.getName());
+    //   console.log('Image URL: ' + profile.getImageUrl());
+    //   console.log('Email: ' + profile.getEmail());
+    // }
   }
 }
