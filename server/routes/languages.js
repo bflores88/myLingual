9b82d48830264b2697a1c954c549585bfb7d9748
Luke Fiorio <lukefiorio@Lukes-MBP.lan>
2019-06-18 10:42:03 -1000

    routes to PUT primary target language & GET all languages

--- server/routes/languages.js
+++ server/routes/languages.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
-const Deck = require('../database/models/Deck');
 const Language = require('../database/models/Language');
-
 const UserLanguage = require('../database/models/UserLanguage');
+const authGuard = require('../guards/authGuard');
+const languagesGuard = require('../guards/languagesGuard');
 
 router.route('/all').get((req, res) => {
   new Language()
@@ -15,6 +15,74 @@ router.route('/all').get((req, res) => {
     })
     .catch((err) => {
       console.log('error', err);
+    });
+});
+
+router.route('/').get(authGuard, (req, res) => {
+  UserLanguage.where({ user_id: req.user.id, active: true })
+    .orderBy('active', 'DESC')
+    .orderBy('language_type', 'ASC')
+    .orderBy('primary', 'DESC')
+    .orderBy('id', 'ASC')
+    .fetchAll({ withRelated: ['languages'] })
+    .then((result) => {
+      // respond with all active payment cards, sorted
+      return res.json(result);
+    })
+    .catch((err) => {
+      console.log('error:', err);
+      return res.status(500).send('Server error');
+    });
+});
+
+// toggle primary target language
+router.route('/:id').put(authGuard, languagesGuard, (req, res) => {
+  // get all of user's target languages
+  UserLanguage.where({ user_id: req.user.id, active: true, language_type: 'target' })
+    .fetchAll()
+    .then((result) => {
+      if (result.length < 1) {
+        throw new Error('Must have at least one active target language to toggle primary.');
+      }
+      // return primary target language (next: update 'primary' to false)
+      return UserLanguage.where({ user_id: req.user.id, primary: true, language_type: 'target' }).fetch();
+    })
+    .then((result) => {
+      // if trying to set primary target to primary: exit out.
+      if (result.id === parseInt(req.params.id)) {
+        throw new Error('This language is already set to primary');
+      }
+      // return prior primary target to false (next: update requested 'primary' to true)
+      return new UserLanguage('id', result.id).save({
+        primary: false,
+      });
+    })
+    .then(() => {
+      // return requested target as primrary (next: get all of user's target languages)
+      return new UserLanguage('id', parseInt(req.params.id)).save({
+        primary: true,
+      });
+    })
+    .then(() => {
+      return UserLanguage.where({ user_id: req.user.id, active: true, language_type: 'target' })
+        .orderBy('active', 'DESC')
+        .orderBy('primary', 'DESC')
+        .orderBy('id', 'ASC')
+        .fetchAll({ withRelated: ['languages'] });
+    })
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      if (
+        err.message === 'Must have at least one active target language to toggle primary.' ||
+        err.message === 'This language is already set to primary'
+      ) {
+        return res.status(400).send('Bad request');
+      } else {
+        return res.status(500).send('Server error');
+      }
     });
 });
 
