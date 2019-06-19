35341445f79317412f0eaa42a4334c44e64e1e0f
Luke Fiorio <lukefiorio@Lukes-MBP.lan>
2019-06-18 17:21:02 -1000

    initial websocket integration

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend.services';
 import { ActivatedRoute } from '@angular/router';
 import { SessionService } from 'src/app/services/session.service';
+import { SocketService } from 'src/app/services/socket.service';
+import { Injectable } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 interface MessageData {
   message_id: number;
@@ -12,6 +15,7 @@ interface MessageData {
   created_at: Date;
 }
 
+@Injectable()
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -19,9 +23,11 @@ interface MessageData {
 })
 export class MessagesComponent implements OnInit {
   messages: MessageData[] = [];
-  messageBody: string = '';
+  messageBody: string;
+  msg: Observable<any>;
+  private msgSub: Subscription;
 
-  userID = 0;
+  userId = 0;
 
   user: {
     id: number;
@@ -40,28 +46,59 @@ export class MessagesComponent implements OnInit {
     target_languages: object;
   };
 
-  constructor(private backend: BackendService, private route: ActivatedRoute, private session: SessionService) {}
+  constructor(
+    private backend: BackendService,
+    private route: ActivatedRoute,
+    private session: SessionService,
+    private socketService: SocketService,
+  ) {
+    this.socketService.getMessage().subscribe((message) => {
+      console.log('User data', message);
+      this.messages.push(message);
+    });
+  }
 
   ngOnInit() {
+    this.msgSub = this.socketService.msg.subscribe((msg) => (this.messageBody = msg));
     let user = this.session.getSession();
-    this.userID = parseInt(user.id);
+    this.userId = parseInt(user.id);
 
-    console.log(this.userID);
+    console.log(this.userId);
+
+    this.socketService.sendIdentity(this.userId);
 
     const id = this.route.snapshot.paramMap.get('id');
     this.backend.getMessages(id).then((data: MessageData[]) => {
       this.messages = data;
-      console.log(this.messages);
+      console.log('**************', this.messages);
     });
+
+    // this.socketService.getMessage().subscribe(
+    //   msg => this.messageBody = msg;
+    //   console.log('new message', this.messageBody);
+    // );
+    // this.sendMessage(msg);
   }
 
-  sendMessage() {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.backend.sendMessage(id, this.messageBody).then(() => {
-      this.backend.getMessages(id).then((data: MessageData[]) => {
-        this.messages = data;
-        this.messageBody = '';
-      });
-    });
+  sendMessage(message) {
+    // const id = this.route.snapshot.paramMap.get('id');
+    this.socketService.sendMessage(this.userId, message);
+    console.log('backend service; sent msg');
+  }
+
+  // sendMessage(message) {
+  //   const id = this.route.snapshot.paramMap.get('id');
+  //   this.backend.sendMessage(id, message).then(() => {
+  //     this.socketService.sendMessage(message);
+  //     this.backend.getMessages(id).then((data: MessageData[]) => {
+  //       this.messages = data;
+  //       message = '';
+  //       console.log('backend service; sent msg');
+  //     });
+  //   });
+  // }
+
+  getMessage() {
+    return this.socketService.getMessage();
   }
 }
