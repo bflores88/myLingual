5bb243c67d550d60d1bdeffead58aba7081e2b98
kmietotwy <kmietotwy@gmail.com>
2019-06-19 11:30:04 -1000

    if user is logged in and goes to splash page, reroutes to home page.  if not logged in and goes to splash page, goes to login page

--- src/app/pages/splash/splash.component.ts
+++ src/app/pages/splash/splash.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.component.html',
-  styleUrls: ['./splash.component.scss']
+  styleUrls: ['./splash.component.scss'],
 })
 export class SplashComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private session: SessionService) {}
 
   ngOnInit() {
-    setTimeout(() => {
-      this.router.navigate(['login']);
-  },1500);
+    if (this.session.isLoggedIn()) {
+      setTimeout(() => {
+        this.router.navigate(['home']);
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        this.router.navigate(['login']);
+      }, 1000);
+    }
   }
-
 }
