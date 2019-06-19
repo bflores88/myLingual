0bf69a79176c21e2a4f765dab946c43a05042ec1
Luke Fiorio <lukefiorio@Lukes-MacBook-Pro.local>
2019-06-19 00:31:06 -1000

    add spanish n

--- server/database/seeds/01_languages.js
+++ server/database/seeds/01_languages.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('languages').insert([
-        { english_name: 'spanish', native_name: 'espanol', language_code: 'es' },
+        { english_name: 'spanish', native_name: 'español', language_code: 'es' },
         { english_name: 'italian', native_name: 'italiano', language_code: 'it' },
         { english_name: 'english', native_name: 'english', language_code: 'en' },
       ]);
