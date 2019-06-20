ead241deb5e4a81064cb416931e3e400c7a49da0
brett <brett_matsumoto@yahoo.com>
2019-06-20 09:46:31 -1000

    added back constructor/removed swUpdate

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,28 +1,23 @@
 import { Component } from '@angular/core';
 import { BackendService } from '../services/backend.services';
 import { Router } from '@angular/router';
-// import { SwUpdate } from '@angular/service-worker';
 
 // interface ClientIdResponse {
 //   client_id: string
 // }
 
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'myLingual';
   client_id: string;
 
-  constructor() { }
-
+  constructor(private router: Router, private backend: BackendService) {}
 
   ngOnInit() {
-    // this.swUpdate.checkForUpdate()
+    // this.swUpdate.checkForUpdate();
   }
-
 }
