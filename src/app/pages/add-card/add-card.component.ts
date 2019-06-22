bb0679e025156767c7e76e5f8faacd7f0cd541d1
brett <brett_matsumoto@yahoo.com>
2019-06-22 10:45:36 -1000

    added function to check operating system

--- src/app/pages/add-card/add-card.component.ts
+++ src/app/pages/add-card/add-card.component.ts
@@ -9,8 +9,30 @@ import { Router } from '@angular/router';
 export class AddCardComponent implements OnInit {
   constructor(public router: Router) {}
 
+  // platform: string = '';
+  getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor;
+ 
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+      return 'Windows Phone';
+    }
+ 
+    if (/android/i.test(userAgent)) {
+      return 'Android';
+    }
+ 
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return 'iOS';
+    }
+ 
+    return 'unknown';
+  }
+
+  
   ngOnInit() {
-    // console.log(navigator.onLine)
+    console.log(this.getMobileOperatingSystem());
   }
 
   continue() {
