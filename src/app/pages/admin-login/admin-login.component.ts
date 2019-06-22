0eacc3c5329247788801f48bdb32a6a578b8e412
Frank <fr.heggeness@gmail.com>
2019-06-21 14:32:44 -1000

    removed lingot store, slight tweaks

---
+++ src/app/pages/admin-login/admin-login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.services';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.scss'],
+})
+export class AdminLoginComponent implements OnInit {
+  formData: {
+    username: string;
+    password: string;
+  } = {
+    username: '',
+    password: '',
+  };
+
+  constructor(private auth: AuthService, private router: Router, private backend: BackendService) {}
+
+  login() {
+    this.auth
+      .login(this.formData)
+      .then((response) => {
+        const { redirectUrl } = this.auth;
+        if (redirectUrl) {
+          this.router.navigate([redirectUrl]);
+          this.auth.redirectUrl = '';
+        } else {
+          //redirects to the home page
+          this.router.navigate(['/home']);
+        }
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+  }
+
+  googleSignin() {
+    return this.backend.googleLogin();
+  }
+
+  ngOnInit() {}
+}
