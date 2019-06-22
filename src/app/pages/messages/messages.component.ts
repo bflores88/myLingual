01f300a57f2192ba1057e2d0b2e2245b608baa14
kmietotwy <kmietotwy@gmail.com>
2019-06-22 11:31:07 -1000

    added send button to messages

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -23,10 +23,10 @@ interface MessageData {
 })
 export class MessagesComponent implements OnInit {
   messages: MessageData[] = [];
-  messageBody: string;
   msg: Observable<any>;
   private msgSub: Subscription;
   newMessages = [];
+  messageBody = '';
 
   userId = 0;
   roomId = 0;
@@ -63,6 +63,7 @@ export class MessagesComponent implements OnInit {
     let user = this.session.getSession();
     this.userId = parseInt(user.id);
     this.username = user.username;
+    
 
     this.socketService.sendIdentity(this.userId);
 
@@ -82,6 +83,8 @@ export class MessagesComponent implements OnInit {
     this.msgSub = this.socketService.msg.subscribe((msg) => {
       this.messages.unshift(msg);
     });
+
+   
   }
 
   sendMessage(message) {
