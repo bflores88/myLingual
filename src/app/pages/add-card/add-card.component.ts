1f3ad6f97403d8d8165691a6116b5e1e5aa72a98
kmietotwy <kmietotwy@gmail.com>
2019-06-22 15:10:57 -1000

    refactored to remove error

--- src/app/pages/add-card/add-card.component.ts
+++ src/app/pages/add-card/add-card.component.ts
@@ -23,7 +23,7 @@ export class AddCardComponent implements OnInit {
     }
  
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window['MSStream']) {
       return 'iOS';
     }
  
